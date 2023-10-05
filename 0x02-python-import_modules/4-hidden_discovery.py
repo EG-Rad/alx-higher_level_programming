746d0376f91e0d1d3e36015926cd5088025f1384
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-05 07:50:51 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-import importlib.util
-import sys
+import hidden_4
 
-# Load the compiled module
-module_name = 'hidden_4'
-module_path = 'hidden_4.pyc'
-spec = importlib.util.spec_from_file_location(module_name, module_path)
-compiled_module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(compiled_module)
 
-# Get the names defined in the module
-module_names = [name for name in dir(compiled_module) if not name.startswith("__")]
+def print_names():
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
 
-# Print the names in alphabetical order
-for name in sorted(module_names):
-    print(name)
+
+if __name__ == "__main__":
+    print_names()
