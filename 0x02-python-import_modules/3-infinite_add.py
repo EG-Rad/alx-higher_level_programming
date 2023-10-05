702cf15d82091895fda16a78a6665f0bb6c5b7dc
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-05 07:48:48 -0700

    3-

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-# Initialize a variable to store the sum
-sum_result = 0
 
-# Iterate over command-line arguments (excluding the script name)
-for arg in sys.argv[1:]:
-    # Convert the argument to an integer and add it to the sum
-    sum_result += int(arg)
+def add_arguments():
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print(total)
 
-# Print the sum
-print(sum_result)
+
+if __name__ == "__main__":
+    add_arguments()
