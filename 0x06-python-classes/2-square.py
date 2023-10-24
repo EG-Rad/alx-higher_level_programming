5263fd7a7fbd12ae096726e5c2ad13b60362a1ac
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-24 03:50:13 -0700

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class represents a square with a private attribute called size.
+It provides a constructor to create square instances
+with an optional size parameter.
+
+Attributes:
+    size (int): The size of the square.
+
+"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
