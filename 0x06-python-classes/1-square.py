6a026728683df26974b2326591cf30ffc968c4ad
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-24 03:45:35 -0700

    1-

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class represents a square with a private attribute called size.
+
+Attributes:
+    size (int): The size of the square.
+
+"""
+
+
 class Square:
     """
     This class represents a square.
