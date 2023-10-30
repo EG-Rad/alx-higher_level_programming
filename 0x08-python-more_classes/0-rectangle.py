bd95512a3c4bed82fd6923f24a69af48aba4b163
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-30 08:41:29 -0700

    00

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 class Rectangle:
-    """This class defines a basic Rectangle."""
 
-    def __init__(self):
-        """Initializes a new instance of a Rectangle."""
-        pass
+    pass
