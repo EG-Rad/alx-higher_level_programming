206e8f50dc1d76f5e9a0ced1cc778cd5890d739c
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-31 05:08:25 -0700

    30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+"""LockedClass module
+
+This module contains the LockedClass class definition.
+The LockedClass restricts dynamically creating new instance
+attributes except for 'first_name'.
+
+Classes:
+    LockedClass: A class that restricts dynamically
+    creating new instance attributes.
+
+Example:
+    lc = LockedClass()
+    lc.first_name = "John"  # Allows setting 'first_name'
+    lc.last_name = "Snow"  # Raises an AttributeError due to the restriction
+"""
+
+
 class LockedClass:
+    """Class that restricts dynamically creating new instance attributes,
+    except 'first_name'."""
+
     __slots__ = ['first_name']
