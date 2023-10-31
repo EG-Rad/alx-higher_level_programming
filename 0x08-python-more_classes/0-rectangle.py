c73e9b6f8fc3c56c7d065be8d1bccf50362d2ac2
Alaa Radwan <alaa.a.radwan@gmail.com>
2023-10-31 01:18:50 -0700

    0-

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
-class Rectangle:
+"""Rectangle module
 
-    pass
+This module defines the Rectangle class representing a geometric rectangle.
+
+Classes:
+    Rectangle: Represents a basic rectangle shape.
+
+"""
+
+
+class Rectangle:
+    """Represents a basic rectangle shape."""
+
+    def __init__(self):
+        """Initializes a new instance of a Rectangle.
+
+        The Rectangle class represents a geometric
+        rectangle shape. This initialization
+        sets up the instance of the rectangle without any
+        specific width or height.
+
+        Args:
+            No arguments needed for initialization.
+        """
+        pass
